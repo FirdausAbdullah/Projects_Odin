8bbac67852fd34d85be7498b65e21c8e8ef3a8b9
Firdaus Abdullah <87492065+FirdausAbdullah@users.noreply.github.com>
2025-12-20 23:22:57 +0800

    Start Library mini project

---
+++ Library/script.js
@@ -0,0 +1,35 @@
+function Book(uuid,title,author,pages,isRead){
+    this.title= title,
+    this.author = author,
+    this.pages = pages,
+    this.isRead = isRead,
+    this.uuid = uuid,
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.isRead)? "Read" : "Not Read"}` ;
+    }
+};
+
+
+const myLibrary = [];
+
+function addBookToLibrary(title,author,pages,isRead){
+    const uuid = crypto.randomUUID();
+    const createdBook = new Book(uuid,title,author,pages,isRead);
+    myLibrary.push(createdBook);
+}
+
+function displayBook(libraryArr){
+    libraryArr.forEach(eachBook => {
+        console.dir(eachBook);
+    });
+}
+
+addBookToLibrary("CSI:Miami","John X",333,true);
+addBookToLibrary("Spy Craft","Mel Grissom",27,true);
+addBookToLibrary("How Can I?","James Rain",731,false);
+addBookToLibrary("Invincible","Scott T. Helberg",90,true);
+addBookToLibrary("Title Kills","Sara Keef",109,false);
+
+
+displayBook(myLibrary);
